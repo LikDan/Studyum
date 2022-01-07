f3138e112afaa9d314cb190072ac4765fc0da24f
LikCoD <likdan.company@gmail.com>
2022-01-07 21:22:32 +0300

    Create user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
 import {Component} from '@angular/core';
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {SERVER_URL} from "../main";
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>'
+  templateUrl: "app.component.html"
 })
 
 
 export class AppComponent {
+
+  user: User | undefined
+
+  constructor(private router: Router, private http: HttpClient) {
+    let username = localStorage.getItem("username")
+    let token = localStorage.getItem("token")
+
+    if (username == undefined || token == undefined){
+      router.navigateByUrl("schedule/login")
+      return
+    }
+
+    http.get<User>(SERVER_URL + "/getUserViaToken?username=" + username + "&token=" + token).subscribe((user: User) => {
+      if (user == undefined){
+        router.navigateByUrl("schedule/login")
+        return
+      }
+
+      this.user = user
+    })
+  }
+
+  mySchedule(): void{
+    if (!this.user){
+      this.router.navigateByUrl("schedule/login")
+      return
+    }
+
+    localStorage.setItem("studyPlaceId", this.user.studyPlaceId.toString())
+    localStorage.setItem("type", this.user.type)
+    localStorage.setItem("name", this.user.name)
+
+    if (this.router.url == "/schedule"){
+      window.location.reload()
+    }else {
+      this.router.navigateByUrl("schedule")
+    }
+  }
+}
+
+interface User{
+  name: string
+  studyPlaceId: number
+  type: string
+  username: string
 }

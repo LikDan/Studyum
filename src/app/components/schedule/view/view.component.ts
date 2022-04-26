bed224864184b22bd1f64287af397d72bc3d514b
= <likdan.company@gmail.com>
2022-04-26 17:16:22 +0300

    Fix schedule height

--- src/app/components/schedule/view/view.component.ts
+++ src/app/components/schedule/view/view.component.ts
@@ -84,8 +84,4 @@ export class ViewComponent implements OnInit {
     return (((lesson.endDate.hours() * 60 + lesson.endDate.minutes()) - (lesson.startDate.hours() * 60 + lesson.startDate.minutes())) * 2 + 'px')
   }
 
-  onScroll(lessonsContainer: any, weekDaysContainer: any) {
-    weekDaysContainer.scrollLeft = lessonsContainer.scrollLeft
-  }
-
 }
